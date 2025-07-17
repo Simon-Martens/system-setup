80a87ba39c80e965eabfe810ab5edcadc235168d
Simon Martens <simon.martens@mailbox.org>
2025-07-17 04:02:57 +0200

    no rebase on conflicts

--- install/4-config.sh
+++ install/4-config.sh
@@ -20,6 +20,7 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global pull.rebase false
 
 # WARNING 
 # You'll need to import the appropriate signing key
