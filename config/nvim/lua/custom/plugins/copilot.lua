4de4793727498404d11920ca3446897079c10b5a
Simon Martens <simon.martens@mailbox.org>
2025-07-17 21:44:25 +0200

    governor-menu

--- config/nvim/lua/custom/plugins/copilot.lua
+++ config/nvim/lua/custom/plugins/copilot.lua
@@ -6,9 +6,5 @@ return {
   config = function()
     -- Use Ctrl + u to accept GitHub Copilot suggestions
     vim.api.nvim_set_keymap('i', '<C-U>', 'copilot#Accept("<CR>")', { expr = true, noremap = false, silent = true })
-    -- vim.api.nvim_set_keymap('i', '<C-I>', '<Plug>(copilot-accept-word)', { expr = true, noremap = false, silent = true })
-    -- vim.api.nvim_set_keymap('i', '<C-n>', '<Plug>(copilot-next)', { expr = true, noremap = false, silent = true })
-    -- vim.api.nvim_set_keymap('i', '<C-N>', '<Plug>(copilot-previous)', { expr = true, noremap = false, silent = true })
-    -- vim.api.nvim_set_keymap('i', '<C-s>', '<Plug>(copilot-suggest)', { expr = true, noremap = false, silent = true })
   end,
 }
