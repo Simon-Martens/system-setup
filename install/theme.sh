cc3bbe3114cc0503f7ceb590e2fb1206059dca76
Simon Martens <simon.martens@mailbox.org>
2025-07-17 03:52:12 +0200

    theme install simplifified

--- install/theme.sh
+++ install/theme.sh
@@ -1,3 +1,13 @@
+# Background Image downloading
+BACKGROUNDS_DIR=~/.config/omarchy/backgrounds/
+
+download_background_image() {
+  local url="$1"
+  local path="$2"
+  gum spin --title "Downloading $url as $path..." -- curl -sL -o "$BACKGROUNDS_DIR/$path" "$url"
+}
+
+for t in ~/.local/share/omarchy/themes/*; do source "$t/backgrounds.sh"; done
 # Use dark mode for QT apps too (like kdenlive)
 sudo pacman -S --noconfirm kvantum-qt5
 
