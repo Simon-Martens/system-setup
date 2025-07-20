f628f2806431c5161630e6133cbf167dce213d49
Simon Martens <simon.martens@mailbox.org>
2025-07-20 19:54:20 +0200

    Just get all the icons from homarr-dashboard-icons

--- install/elixir.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-yay -S --noconfirm --needed elixir erlang-dev erlang-asn1 erlang-ssl elixir-ls
---
+++ install/icons.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf /tmp/dashboard-icons
+if [ ! -d "/tmp/dashboard-icons" ]; then
+	git clone --branch main --depth 1 https://github.com/homarr-labs/dashboard-icons /tmp/dashboard-icons
+fi 
+if [ -d "/tmp/dashboard-icons" ]; then
+	mv /tmp/dashboard-icons/png/* "$HOME/.local/share/icons/hicolor/48x48/apps/"
+	gtk4-update-icon-cache "$HOME/.local/share/icons/hicolor" &> /dev/null
+fi
