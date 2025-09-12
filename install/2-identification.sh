4adac546e5c0f111997e4c754669728faad2c5de
Simon Martens <simon.martens@mailbox.org>
2025-09-12 22:32:51 +0200

    BUGFIX: terminal env name

--- install/2-identification.sh
+++ install/2-identification.sh
@@ -3,5 +3,5 @@ yay -S --noconfirm --needed gum
 
 # Configure identification
 echo -e "\nEnter identification for git and autocomplete..."
-export system-setup_USER_NAME=$(gum input --placeholder "Enter full name" --prompt "Name> ")
-export system-setup_USER_EMAIL=$(gum input --placeholder "Enter email address" --prompt "Email> ")
+export system_setup_USER_NAME=$(gum input --placeholder "Enter full name" --prompt "Name> ")
+export system_setup_USER_EMAIL=$(gum input --placeholder "Enter email address" --prompt "Email> ")
