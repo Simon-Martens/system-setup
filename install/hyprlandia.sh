1f7ed5554fc373e5d88edc4f9ba508d034982966
Simon Martens <simon.martens@mailbox.org>
2025-10-09 01:48:59 +0200

    hyprpm install hyprscrolling

--- install/hyprlandia.sh
+++ install/hyprlandia.sh
@@ -3,5 +3,8 @@ yay -S --noconfirm --needed \
   wofi waybar swaync swaybg uwsm libnewt \
   xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
 
+hyprpm update
+hyprpm add https://github.com/hyprwm/hyprland-plugins
+
 # Start Hyprland on first session
 # echo "[[ -z \$DISPLAY && \$(tty) == /dev/tty1 ]] && exec Hyprland" >~/.bash_profile
