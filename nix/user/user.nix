d070a4190e9045ffe8ce5f51d5b0fddb92d281d6
Simon Martens <simon.martens@mailbox.org>
2025-10-18 00:17:16 +0200

    better hyprland things

--- nix/user/user.nix
+++ nix/user/user.nix
@@ -21,19 +21,18 @@
 		};
 	};
 
-	let
-		adwaita-cursors = pkgs.fetchurl {
-			url = "https://github.com/manu-mannattil/adwaita-cursors/releases/download/v1.2/adwaita-cursors.tar.gz";
-			sha256 = "sha256-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=";
-		};
-		
-		extracted = pkgs.runCommand "adwaita-cursors-extracted" {} ''
-			mkdir -p $out
-			tar xzf ${adwaita-cursors} -C $out
-		'';
-	in {
-		home.file.".icons/default/cursors" = {
-			source = "${extracted}/adwaita-cursors/Adwaita/cursors";
-			recursive = true;
-		};
+home.file.".icons/default/cursors" = let
+    adwaita-cursors = pkgs.fetchurl {
+      url = "https://github.com/manu-mannattil/adwaita-cursors/releases/download/v1.2/adwaita-cursors.tar.gz";
+      sha256 = "sha256-zDahuX7l0aGb+UKH1glnWBLVZHkMRTWQ1XlmxR8A/r8=";
+    };
+    
+    extracted = pkgs.runCommand "adwaita-cursors-extracted" {} ''
+      mkdir -p $out
+      tar xzf ${adwaita-cursors} -C $out
+    '';
+  in {
+    source = "${extracted}/adwaita-cursors/Adwaita/cursors";
+    recursive = true;
+  };
 }
