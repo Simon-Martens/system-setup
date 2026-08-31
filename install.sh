a628cf5b8fee7baaef9fa989316b59549b6494d7
Simon Martens <simon.martens@mailbox.org>
2026-08-31 07:24:40 +0200

    +btop

--- install.sh
+++ install.sh
@@ -6,8 +6,12 @@ sudo dnf copr enable lionheartp/Hyprland # Gives hyprland & utilse. Isntalls kit
 sudo dnf copr enable avengemedia/dms # Gives DankMaterialShell (stable)
 sudo dnf copr enable jdxcode/mise # Gives mise
 sudo dnf copr enable imput/helium # gives helium
-sudo dnf install alacritty nvim atuin zoxide stow mise helium hyprland mako dms tmux fd --refresh -y
+sudo dnf install btop alacritty nvim atuin zoxide stow mise helium hyprland mako dms tmux fd --refresh -y
 
 
 systemctl enable --user dms
 mise install
+
+# Then the user might:
+# - install webapps (through scripts)
+# - stow directory/ in dotfiles
