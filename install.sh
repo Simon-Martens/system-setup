5912d21b6d3a63a01a477ec90f8eeeadaac84b99
Simon Martens <simon.martens@mailbox.org>
2026-09-07 00:09:21 +0200

    +dcal, dms-greeter

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ sudo dnf copr enable lionheartp/Hyprland # Gives hyprland & utilse. Isntalls kit
 sudo dnf copr enable avengemedia/dms # Gives DankMaterialShell (stable)
 sudo dnf copr enable jdxcode/mise # Gives mise
 sudo dnf copr enable imput/helium # gives helium
-sudo dnf install btop alacritty nvim atuin zoxide stow mise helium hyprland hyprland-guiutils mako dms tmux fd dsearch --refresh -y
+sudo dnf install btop alacritty nvim atuin zoxide stow mise helium hyprland hyprland-guiutils mako dms dcal dms-greeter tmux fd dsearch --refresh -y
 
-mise install
+mise install # Installl all programming utilities and packages
 
 systemctl enable --user dms # Desktoip (Dank Material Shell) 
 systemctl enable --user dsearch # Search utility for DMS
