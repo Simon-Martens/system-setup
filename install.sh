eedbaeb72b9ec87c7d1270b5001e0c06204d0123
Simon Martens <simon.martens@mailbox.org>
2026-09-07 00:01:07 +0200

    +More stuff

--- install.sh
+++ install.sh
@@ -6,11 +6,12 @@ sudo dnf copr enable lionheartp/Hyprland # Gives hyprland & utilse. Isntalls kit
 sudo dnf copr enable avengemedia/dms # Gives DankMaterialShell (stable)
 sudo dnf copr enable jdxcode/mise # Gives mise
 sudo dnf copr enable imput/helium # gives helium
-sudo dnf install btop alacritty nvim atuin zoxide stow mise helium hyprland hyprland-guiutils mako dms tmux fd --refresh -y
+sudo dnf install btop alacritty nvim atuin zoxide stow mise helium hyprland hyprland-guiutils mako dms tmux fd dsearch --refresh -y
 
-
-systemctl enable --user dms
 mise install
+
+systemctl enable --user dms # Desktoip (Dank Material Shell) 
+systemctl enable --user dsearch # Search utility for DMS
 
 # Then the user might:
 # - install webapps (through scripts)
